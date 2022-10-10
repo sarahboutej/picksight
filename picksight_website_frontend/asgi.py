899a66d8dea10c3480a619f094832a08c1d5dcdd
saraboutej <sboutej@exoplatform.com>
2022-10-10 08:15:02 +0100

    Add new Django application tempplates

---
+++ picksight_website_frontend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for picksight_website_frontend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picksight_website_frontend.settings')
+
+application = get_asgi_application()
